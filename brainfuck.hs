2031f742d24d2ea3922f650fda50f3bfaee25185
Sage Raflik <neonfuz@gmail.com>
2016-09-29 13:38:58 -0500

    Attempt to make loops generic, failed

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,5 +1,6 @@
 import Tape
 import Data.Char (ord, chr)
+import Data.Maybe (fromMaybe)
 
 bfCompile :: String -> Tape Char
 bfCompile program = tapeFromList $ filter (`elem` "+-<>[],.") program ++ "E"
@@ -18,21 +19,15 @@ bf prog mem stdIn stdOut =
     '>' -> bf (next prog) (next mem) stdIn stdOut
     ',' -> bf (next prog) (set mem $ ord $ head stdIn) (tail stdIn) stdOut
     '.' -> bf (next prog) mem stdIn (chr (current mem) : stdOut)
-    '[' -> bf (if current mem == 0 then (loopForward (next prog) 1) else (next prog)) mem stdIn stdOut
-    ']' -> bf (if current mem /= 0 then (loopBackward (prev prog) 1) else (next prog)) mem stdIn stdOut
+    '[' -> bf (if current mem == 0 then (loop forward (next prog) 1) else (next prog)) mem stdIn stdOut
+    ']' -> bf (if current mem /= 0 then (loop backward (next prog) 1) else (next prog)) mem stdIn stdOut
     _ -> "unhandled"
   where set mem val = case mem of (Tape b c e) -> Tape b val e
         increment mem = set mem (current mem + 1)
         decrement mem = set mem (current mem - 1)
-        loopForward prog count = case (prog, count) of
-          (prog, 0) -> next prog
-          (prog@(Tape _ c _), count) -> case c of
-            '[' -> loopBackward (next prog) (count + 1)
-            ']' -> loopBackward (next prog) (count - 1)
-            _   -> loopBackward (next prog) count
-        loopBackward prog count = case (prog, count) of
-          (prog, 0) -> next prog
-          (prog@(Tape _ c _), count) -> case c of
-            '[' -> loopBackward (prev prog) (count - 1)
-            ']' -> loopBackward (prev prog) (count + 1)
-            _   -> loopBackward (prev prog) count
+        forward  = (next, [('[', 1), (']', -1)])
+        backward = (prev, [('[', -1), (']', 1)])
+        loop direction prog count = case (prog, count) of
+          (prog, 0) -> fst direction prog
+          (prog@(Tape _ c _), count) ->
+            loop direction (fst direction prog) (count + (fromMaybe 0 $ lookup c $ snd direction))
