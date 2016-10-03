1bdaad1d440569432fcc6ee7fa7cc9b7811915ef
Sage Raflik <neonfuz@gmail.com>
2016-10-03 04:17:08 -0500

    Fixed logical errors

--- brainfuck.hs
+++ brainfuck.hs
@@ -20,7 +20,7 @@ bf prog mem stdIn stdOut =
     ',' -> bf (next prog) (set mem $ ord $ head stdIn) (tail stdIn) stdOut
     '.' -> bf (next prog) mem stdIn (chr (current mem) : stdOut)
     '[' -> bf (if current mem == 0 then (loop forward (next prog) 1) else (next prog)) mem stdIn stdOut
-    ']' -> bf (if current mem /= 0 then (loop backward (next prog) 1) else (next prog)) mem stdIn stdOut
+    ']' -> bf (if current mem /= 0 then (loop backward (prev prog) 1) else (next prog)) mem stdIn stdOut
     _ -> "unhandled"
   where set mem val = case mem of (Tape b c e) -> Tape b val e
         increment mem = set mem $ (current mem + 1 + 256) `mod` 256
@@ -28,6 +28,6 @@ bf prog mem stdIn stdOut =
         forward  = (next, [('[', 1), (']', -1)])
         backward = (prev, [('[', -1), (']', 1)])
         loop direction prog count = case (prog, count) of
-          (prog, 0) -> fst direction prog
+          (prog, 0) -> next prog
           (prog@(Tape _ c _), count) ->
             loop direction (fst direction prog) (count + (fromMaybe 0 $ lookup c $ snd direction))
